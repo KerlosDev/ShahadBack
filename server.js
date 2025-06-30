4863863b8a30e64843134c6920eba5dbfe6330dd
kerloshany3 <kerlostablet@gmail.com>
2025-06-30 03:47:27 +0300

    s

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const bookOrderRoutes = require("./routes/bookOrderRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 dbconnection();
 
